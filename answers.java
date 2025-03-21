f9d422332d0fda0b6fe559b04952f02c99e68455
saad <saadvhere007@gmail.com>
2025-03-21 15:43:50 -0400

    did till q13

---
+++ answers.java
@@ -0,0 +1,54 @@
+
+// q1, afternoon night time 
+
+// q2, Person[] people;
+
+// q3, boolean[] vacant;
+
+// q4 , once
+
+// q5 , correction, int[] counts;   
+//                  boolean[] occupied;
+
+// q6 , double[] readings = new double[60];
+//    String[] urls = new String[90];
+//    TicketMachine[] machines = new TicketMachine[5];
+
+// q7 , 20 String objects
+
+// q8 , double[] prices = new double[50];
+
+// q9 , 
+
+/**
+ * Write a description of class answers here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class answers
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class answers
+     */
+    public answers()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
